e9a8b746d11bf6723a4506602d7dbd71007c3da5
ycjunhua <ycjunhua@163.com>
2016-09-09 15:02:48 +0800

    done the dependentpicker

--- iOS-Study-OC/iOS-Study-OC/DependentPickerViewController.h
+++ iOS-Study-OC/iOS-Study-OC/DependentPickerViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface DependentPickerViewController : UIViewController
-
+    <UIPickerViewDelegate, UIPickerViewDataSource>
 @end
--- iOS-Study-OC/iOS-Study-OC/DependentPickerViewController.m
+++ iOS-Study-OC/iOS-Study-OC/DependentPickerViewController.m
@@ -10,6 +10,10 @@
 
 @interface DependentPickerViewController ()
 
+@property (weak, nonatomic) IBOutlet UIPickerView *dependentPicker;
+@property (strong, nonatomic) NSDictionary *dic;
+@property (strong, nonatomic) NSArray *names1;
+@property (strong, nonatomic) NSArray *names2;
 @end
 
 @implementation DependentPickerViewController
@@ -17,11 +21,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSURL *url = [bundle URLForResource:@"statedictionary" withExtension:@"plist"];
+    _dic = [NSDictionary dictionaryWithContentsOfURL:url];
+    NSArray *allKeys = [_dic allKeys];
+    _names1 = [allKeys sortedArrayUsingSelector:@selector(compare:)];
+    _names2 = _dic[_names1[0]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonPress:(id)sender {
+    NSString *str1 = _names1[[_dependentPicker selectedRowInComponent:0]];
+    NSString *str2 = _names2[[_dependentPicker selectedRowInComponent:1]];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:str1 message:str2 preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:action];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 2;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    if(component == 0){
+        return [_names1 count];
+    }
+    else{
+        return [_names2 count];
+    }
+}
+
+#pragma mark Picker Delegate Method
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if(component == 0){
+        return _names1[row];
+    }
+    else{
+        return _names2[row];
+    }
+}
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    if(component == 0){
+        NSString *key = _names1[row];
+        _names2 = _dic[key];
+        [_dependentPicker reloadComponent:1];
+        [_dependentPicker selectRow:0 inComponent:1 animated:YES];
+    }
+}
+-(CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component{
+    CGFloat pickerWidth = pickerView.bounds.size.width;
+    if(component == 0){
+        return pickerWidth/3*2;
+    }
+    else{
+        return pickerWidth/3;
+    }
 }
 
 /*
