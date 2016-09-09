fd05f3115b63e01e861d31eea2cd2dc9c91d5d11
ycjunhua <ycjunhua@163.com>
2016-09-09 11:57:50 +0800

    done the double picker

--- iOS-Study-OC/iOS-Study-OC/DoublePickerViewController.h
+++ iOS-Study-OC/iOS-Study-OC/DoublePickerViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface DoublePickerViewController : UIViewController
-
+    <UIPickerViewDelegate, UIPickerViewDataSource>
 @end
--- iOS-Study-OC/iOS-Study-OC/DoublePickerViewController.m
+++ iOS-Study-OC/iOS-Study-OC/DoublePickerViewController.m
@@ -9,6 +9,9 @@
 #import "DoublePickerViewController.h"
 
 @interface DoublePickerViewController ()
+@property (weak, nonatomic) IBOutlet UIPickerView *doublePicker;
+@property (strong, nonatomic) NSArray *names1;
+@property (strong, nonatomic) NSArray *names2;
 
 @end
 
@@ -17,13 +20,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _names1 = @[@"fuck", @"shit", @"bitch", @"asshole"];
+    _names2 = @[@"wemon", @"man", @"renyao"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)buttonPress:(id)sender {
+    NSString* str1 = _names1[[_doublePicker selectedRowInComponent:0]];
+    NSString* str2 = _names2[[_doublePicker selectedRowInComponent:1]];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:str1 message:str2 preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:action];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 2;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    if(component == 0){
+        return [_names1 count];
+    }
+    else{
+        return [_names2 count];
+    }
+}
+
+#pragma mark Picker Delegate Method
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if(component == 0){
+        return _names1[row];
+    }
+    else{
+        return _names2[row];
+    }
+}
 /*
 #pragma mark - Navigation
 
